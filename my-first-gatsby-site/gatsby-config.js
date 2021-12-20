0493f896833af088f23ccfe0f24c7d5b76b3d33f
Todd Bittner <toddbittner@gmail.com>
2021-12-20 10:30:37 -0600

    Created first GraphQL query.

--- my-first-gatsby-site/gatsby-config.js
+++ my-first-gatsby-site/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "My First Gatsby Site",
+    title: "My GraphQL Gatsby Site",
   },
   plugins: [
     "gatsby-plugin-image",
